7d85094d32fe21834d66a6663cf3fbcd441bd154
Mac-An-tSaoir <77882851+Mac-An-tSaoir@users.noreply.github.com>
2022-05-24 16:24:33 +0100

    Initial commit of Spring Boot project

---
+++ src/main/java/com/daveplaces/DaveplacesApplication.java
@@ -0,0 +1,13 @@
+package com.daveplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveplacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaveplacesApplication.class, args);
+	}
+
+}
