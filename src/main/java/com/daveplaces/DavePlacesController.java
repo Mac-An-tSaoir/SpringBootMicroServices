3665b454159cad56089ed9993fa656418ec7c672
Mac-An-tSaoir <77882851+Mac-An-tSaoir@users.noreply.github.com>
2022-05-27 09:49:26 +0100

    Add Javadoc, add default / endpoint

---
+++ src/main/java/com/daveplaces/DavePlacesController.java
@@ -0,0 +1,22 @@
+package com.daveplaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DavePlacesController {
+	
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 */
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+}
