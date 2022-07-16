c9521ae1b6e0f46df8404c136d27032cc5432cb7
Mac-An-tSaoir <77882851+Mac-An-tSaoir@users.noreply.github.com>
2022-07-16 14:16:49 +0100

    @Autowire, @Component, HTTP mtds, explore Model and ModelAndView,

--- src/main/java/com/daveplaces/DavePlacesController.java
+++ src/main/java/com/daveplaces/DavePlacesController.java
@@ -1,21 +1,55 @@
 package com.daveplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.daveplaces.dto.BarcodeDTO;
+import com.daveplaces.service.IBarcodeService;
 
 @Controller
 public class DavePlacesController {
+	
+	@Autowired
+	private IBarcodeService barcodeServiceStub;
 	
 	/**
 	 * Handle the /start endpoint
 	 * @return
 	 */
 	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public String read() {
+	public String read(Model model) {
+		BarcodeDTO barcodeDTO = barcodeServiceStub.fetchByBarcode(9780321502797L);
+		model.addAttribute("barcodeDTO", barcodeDTO);
+		//model.addAttribute(barcodeDTO);)
 		return "start";
 	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers={"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=gold"})
+	public ModelAndView readGold() {
+		BarcodeDTO barcodeDTO = barcodeServiceStub.fetchByBarcode(9780321502797L);
+		barcodeDTO.setBarcode(5333310580227L);
+		barcodeDTO.setType("Dazoo");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");
+		modelAndView.addObject("barcodeDTO", barcodeDTO);
+		return modelAndView;
+	}	
 	
 	@PostMapping("/start")
 	public String create() {
@@ -32,11 +66,11 @@ public class DavePlacesController {
 	}
 	
 	/**
-	 * Handle the /home endpoint
+	 * Handle the /homePage endpoint
 	 * @return
 	 */
-	@PostMapping("/home")
+	@GetMapping("/homePage")
 	public String storeBarcode() {
-		return "Interrogate Barcode";
+		return "homePage";
 	}
 }
