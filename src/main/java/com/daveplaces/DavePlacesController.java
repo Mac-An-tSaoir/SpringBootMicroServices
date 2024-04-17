4b7606701fae7575be9464abd3f4fc0962c7a563
Mac-An-tSaoir <77882851+Mac-An-tSaoir@users.noreply.github.com>
2024-04-17 14:59:46 +0100

    finish v36 pass values from form to Controller,

--- src/main/java/com/daveplaces/DavePlacesController.java
+++ src/main/java/com/daveplaces/DavePlacesController.java
@@ -1,5 +1,7 @@
 package com.daveplaces;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -79,6 +81,22 @@ public class DavePlacesController {
 	public String index() {
 		return "default";
 	}
+	
+	@RequestMapping("/searchPlants")
+	public String searchPlants(@RequestParam(value="searchTerm", required=false, defaultValue="") String searchTerm) {
+		String url_Term = searchTerm + ", ";
+		return "start";
+	}
+	
+	@RequestMapping("/searchPlantsAll")
+	public String searchPlantsAll(@RequestParam Map<String, String> requestParams) {
+		
+		int params = requestParams.size();
+		requestParams.get("searchTerm");
+		return "start";
+	}
+	
+	
 	
 
 }
